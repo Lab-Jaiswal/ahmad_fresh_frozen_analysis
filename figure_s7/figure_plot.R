a8c6a8abc7b1a9a014599e65d8963436210850b2
danielnachun <daniel.nachun@gmail.com>
2023-04-28 15:25:05 -0700

    add T-cell DE

--- figure_s7/figure_plot.R
+++ figure_s7/figure_plot.R
@@ -73,7 +73,8 @@ volcano_plot <- function(top_table, plot_title, logfc_cutoff = NA, cutoff = 0.05
     p
 }
 
-all_samples_limma_tibble <- read_rds("../macrophage_clustering/all_samples_limma_tibble.rda")
+all_samples_limma_tibble_macro <- read_rds("../macrophage_clustering/all_samples_limma_tibble.rda")
+all_samples_limma_tibble_tcell <- read_rds("../t_cell_clustering/all_samples_limma_tibble.rda")
 
 all_samples_annotated <- read_rds("../figure2/all_samples_annotated.rda")
 
@@ -90,21 +91,33 @@ all_samples_metadata$Celltype %<>% str_replace_all("macrophages", fixed("Mφ")) 
 
 figure_a <- qc_boxplot("silhouette_width", all_samples_metadata, x_var = "Celltype", fill_var = "Status",
                        y_label = "Silhouette width", angled_text = TRUE)
-figure_b <- volcano_plot(all_samples_limma_tibble[["LYVE1+ TR macrophages"]], "LYVE1+ TR macrophages")
-figure_c <- volcano_plot(all_samples_limma_tibble[["CD16+ monocytes"]], "CD16+ monocytes")
+figure_b <- volcano_plot(all_samples_limma_tibble_macro[["LYVE1+ TR macrophages"]], "LYVE1+ TR macrophages")
+figure_c <- volcano_plot(all_samples_limma_tibble_macro[["CD16+ monocytes"]], "CD16+ monocytes")
+figure_d <- volcano_plot(all_samples_limma_tibble_tcell[["Active NK cells"]], "Active NK cells")
+figure_e <- volcano_plot(all_samples_limma_tibble_tcell[["Resting NK cells"]], "Resting NK cells")
+figure_f <- volcano_plot(all_samples_limma_tibble_tcell[["CD4+ T-cells"]], "CD4+ T-cells")
+figure_g <- volcano_plot(all_samples_limma_tibble_tcell[["CD8+ T-cells"]], "CD8+ T-cells")
 
 figure <- ggdraw() +
-    draw_plot(figure_a, x = 0.19, y = 0.5, width = 0.64, height = 0.5)  +
-    draw_plot(figure_b, x = 0.01, y = 0.0, width = 0.49, height = 0.5) +
-    draw_plot(figure_c, x = 0.51, y = 0.0, width = 0.49, height = 0.5) +
+    draw_plot(figure_a, x = 0.19, y = 0.75, width = 0.64, height = 0.25)  +
+    draw_plot(figure_b, x = 0.01, y = 0.5, width = 0.49, height = 0.25) +
+    draw_plot(figure_c, x = 0.51, y = 0.5, width = 0.49, height = 0.25) +
+    draw_plot(figure_d, x = 0.01, y = 0.25, width = 0.49, height = 0.25) +
+    draw_plot(figure_e, x = 0.51, y = 0.25, width = 0.49, height = 0.25) +
+    draw_plot(figure_f, x = 0.01, y = 0.0, width = 0.49, height = 0.25) +
+    draw_plot(figure_g, x = 0.51, y = 0.0, width = 0.49, height = 0.25) +
     draw_label("A", size = 15.0, x = 0.18, y = 1.0, hjust = 0.0, vjust = 1.0, fontfamily = "Noto Sans", fontface = "bold") +
-    draw_label("B", size = 15.0, x = 0.0, y = 0.5, hjust = 0.0, vjust = 1.0, fontfamily = "Noto Sans", fontface = "bold") +
-    draw_label("C", size = 15.0, x = 0.5, y = 0.5, hjust = 0.0, vjust = 1.0, fontfamily = "Noto Sans", fontface = "bold")
+    draw_label("B", size = 15.0, x = 0.0, y = 0.75, hjust = 0.0, vjust = 1.0, fontfamily = "Noto Sans", fontface = "bold") +
+    draw_label("C", size = 15.0, x = 0.5, y = 0.75, hjust = 0.0, vjust = 1.0, fontfamily = "Noto Sans", fontface = "bold") +
+    draw_label("D", size = 15.0, x = 0.0, y = 0.5, hjust = 0.0, vjust = 1.0, fontfamily = "Noto Sans", fontface = "bold") +
+    draw_label("E", size = 15.0, x = 0.5, y = 0.5, hjust = 0.0, vjust = 1.0, fontfamily = "Noto Sans", fontface = "bold") +
+    draw_label("F", size = 15.0, x = 0.0, y = 0.25, hjust = 0.0, vjust = 1.0, fontfamily = "Noto Sans", fontface = "bold") +
+    draw_label("G", size = 15.0, x = 0.5, y = 0.25, hjust = 0.0, vjust = 1.0, fontfamily = "Noto Sans", fontface = "bold")
 
 figure_title <- ggdraw() +
     draw_label(label = "Figure S7", x = 0.5, hjust = 0.5, size = 20.0, fontfamily = "Noto Sans", fontface = "bold")
 
 figure_grid <- plot_grid(figure_title, figure, ncol = 1L, rel_heights = c(0.05, 1.0))
 figure_grid_notitle <- plot_grid(figure, ncol = 1L)
-ggsave("figure_s7.pdf", figure_grid, width = 12.0, height = 12.0, units = "in", device = cairo_pdf)
-ggsave("figure_s7_notitle.pdf", figure_grid_notitle, width = 12.0, height = 12.0, units = "in", device = cairo_pdf)
+ggsave("figure_s7.pdf", figure_grid, width = 12.0, height = 24.0, units = "in", device = cairo_pdf)
+ggsave("figure_s7_notitle.pdf", figure_grid_notitle, width = 12.0, height = 24.0, units = "in", device = cairo_pdf)
